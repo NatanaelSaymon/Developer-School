02a271febabe6eb017033430ab5533b872fc4ef7
Natanael Saymon <nsaymonsouza@gmail.com>
2020-05-12 21:41:04 -0300

    menu ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,19 +1,9 @@
-const modalOverlay = document.querySelector('.modal-overlay')
+const currentPage = location.pathname
+const menuItens = document.querySelectorAll("header .links a")
 
-const cards = document.querySelectorAll('.card')
-
-for(let card of cards){
-    card.addEventListener("click", function(){
-        const videoId = card.getAttribute('id') /* Pegar o atributo ID*/
-        window.location.href = `/video?id=${videoId}`
-        // modalOverlay.classList.add('active')
-        // modalOverlay.querySelector('iframe').src = `https://youtube.com/embed/${videoId}`
-    })
+for(item of menuItens){
+    if(currentPage.includes(item.getAttribute("href"))){
+        item.classList.add('active')
+    }
 }
-
-//isnerindo a função de fechar o modal
-// document.querySelector('.close-modal').addEventListener("click", function(){
-//     modalOverlay.classList.remove('active')
-//     modalOverlay.querySelector('iframe').src = "" /* remove o video */
-// })
 
